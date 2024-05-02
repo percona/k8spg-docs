c0f7f399e5a93b325cb457b0501cc098a60afb0a
Alina Derkach <alina.derkach@percona.com>
2024-05-02 18:15:14 +0300

    Update percona-select.js

--- docs/js/percona-select.js
+++ docs/js/percona-select.js
@@ -3,39 +3,42 @@ const selectBox = document.getElementById('versionSelect');
 if (selectBox) {
     // Populate the select box with options based on the versionMap
     const versionMap = {
-        '2.3.1': '/2.3.1/',  
-        '1.5.1': '/1.5.1/',
+        '2.3.1': '/2.0/',  
+        '1.5.1': '/1.0/',
         // Add new versions here as needed
     };
 
+    function getCurrentVersionFromUrl() {
+        for (const path of Object.values(versionMap)) {
+            if (window.location.pathname.includes(path)) {
+                return path;  // Return the matching path as soon as one is found
+            }
+        }
+        return null;  // Return null if no match is found
+    }
+
     Object.keys(versionMap).forEach(version => {
         const option = document.createElement('option');
-        option.value = version;
-        option.textContent = version; // Appending the Unicode right-pointing pointer
+        option.value = versionMap[version];
+        option.textContent = version;
         selectBox.appendChild(option);
     });
 
-    // Function to extract the current version from URL
-    function getCurrentVersionFromUrl() {
-        const regex = /\/(\d+\.\d+)\//;
-        const matches = window.location.href.match(regex);
-        return matches ? matches[1] : null;
-    }
-
     // Set initial selection based on URL
-    const currentVersion = getCurrentVersionFromUrl();
-    if (currentVersion) {
-        selectBox.value = currentVersion;
+    const currentSegment = getCurrentVersionFromUrl();
+    if (currentSegment) {
+        selectBox.value = currentSegment;
     }
 
     // Add event listener for changing URL based on selection
     selectBox.addEventListener('change', function() {
         const selectedVersion = this.value;
-        if (selectedVersion !== getCurrentVersionFromUrl()) {
-            const newUrl = window.location.href.replace(/\/(\d+\.\d+)\//, versionMap[selectedVersion]);
+        const currentSegment = getCurrentVersionFromUrl();
+        if (selectedVersion !== currentSegment) { // Only redirect if the selected version is different
+            const newUrl = window.location.href.replace(currentSegment, selectedVersion);
             window.location.href = newUrl;
         }
     });
 } else {
     console.log("No version selector available on this website.");
-}
+} 
